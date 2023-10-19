1087a3a449ad455915648880a4688b9ff6614bd6
Vladislav <bublik1170@gmail.com>
2023-10-19 17:42:50 +0300

    feat:cart creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_210008) do
-  create_table "products", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.string "image_url"
-    t.decimal "price", precision: 8, scale: 2
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_231_017_223_736) do
+  create_table 'carts', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
+  create_table 'line_items', force: :cascade do |t|
+    t.integer ' product_id', null: false
+    t.integer 'cart_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['cart_id'], name: 'index_line_items_on_cart_id'
+    t.index ['product_id'], name: 'index_line_items_on_product_id'
+  end
+
+  create_table 'products', force: :cascade do |t|
+    t.string 'title'
+    t.text 'description'
+    t.string 'image_url'
+    t.decimal 'price', precision: 8, scale: 2
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  add_foreign_key 'line_items', 'carts'
+  add_foreign_key 'line_items', 'products'
 end
